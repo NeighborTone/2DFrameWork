a28ce31e368ba713f55591dfa493ff56d21b9e99
tonarinohito <rsp44316@gmail.com>
2018-04-19 21:43:49 +0900

    不要な名前空間削除

--- 2DGameFrameWork/Game.cpp
+++ 2DGameFrameWork/Game.cpp
@@ -1,55 +1,54 @@
 #include "Game.h"
-namespace DX9
+
+void Game::Init()
 {
-	void Game::Init()
+	for (int i = 0; i < 16; ++i)
 	{
-		for (int i = 0; i < 16; ++i)
-		{
-			rota[i].image.Load("カーソル.png");
-			rota[i].vec2 = { 0 + (float)i * 80, 200 };
-			rota[i].r = 0;
-		}
-
-		me.image.Load("カーソル.png");
-		me.vec2 = { 0,0 };
+		rota[i].image.Load("カーソル.png");
+		rota[i].vec2 = { 0 + (float)i * 80, 200 };
+		rota[i].r = 0;
 	}
-	void Game::Run()
-	{
-		key.Run();
-		for (int i = 0; i < 16; ++i)
-		{
-			rota[i].r -= 1;
-		}
 
-		if (key.On(KeyBoard::Key::KEY_RIGHT))
-		{
-			me.vec2.x += 5;
-		}
-		if (key.On(KeyBoard::Key::KEY_LEFT))
-		{
-			me.vec2.x -= 5;
-		}
-		if (key.On(KeyBoard::Key::KEY_UP))
-		{
-			me.vec2.y -= 5;
-		}
-		if (key.On(KeyBoard::Key::KEY_DOWN))
-		{
-			me.vec2.y += 5;
-		}
-	}
-	void Game::Draw()
+	me.image.Load("カーソル.png");
+	me.vec2 = { 0,0 };
+}
+void Game::Run()
+{
+	key.Run();
+	for (int i = 0; i < 16; ++i)
 	{
-		for (int i = 0; i < 16; ++i)
-		{
-			rota[i].src = { 0,0,80,80 };
-			rota[i].draw = { 40,40,80,80 };
-			rota[i].image.DrawRota(rota[i].vec2, rota[i].r, rota[i].src, rota[i].draw);
-		}
-		me.image.Draw(me.vec2);
+		rota[i].r -= 1;
 	}
-	void Game::Fin()
-	{
 
+	if (key.On(KeyBoard::Key::KEY_RIGHT))
+	{
+		me.vec2.x += 5;
+	}
+	if (key.On(KeyBoard::Key::KEY_LEFT))
+	{
+		me.vec2.x -= 5;
+	}
+	if (key.On(KeyBoard::Key::KEY_UP))
+	{
+		me.vec2.y -= 5;
+	}
+	if (key.On(KeyBoard::Key::KEY_DOWN))
+	{
+		me.vec2.y += 5;
 	}
 }
+void Game::Draw()
+{
+	for (int i = 0; i < 16; ++i)
+	{
+		rota[i].src = { 0,0,80,80 };
+		rota[i].draw = { 40,40,80,80 };
+		rota[i].image.DrawRota(rota[i].vec2, rota[i].r, rota[i].src, rota[i].draw);
+	}
+	me.image.Draw(me.vec2);
+}
+void Game::Fin()
+{
+
+}
+
--- 2DGameFrameWork/Game.h
+++ 2DGameFrameWork/Game.h
@@ -1,26 +1,24 @@
 #pragma once
 #include "Image.h"
 #include "Keyboard.h"
-namespace DX9
+
+class Game
 {
-	class Game
+public:
+	struct Rota
 	{
-	public:
-		struct Rota
-		{
-			Image image;
-			MyClass::Vec2 vec2;
-			MyClass::Box src;
-			MyClass::Box draw;
-			float r;
-		}rota[16], me;
+		Image image;
+		MyClass::Vec2 vec2;
+		MyClass::Box src;
+		MyClass::Box draw;
+		float r;
+	}rota[16], me;
 
-		KeyBoard key;
-		void Init();
-		void Run();
-		void Draw();
-		void Fin();
+	KeyBoard key;
+	void Init();
+	void Run();
+	void Draw();
+	void Fin();
 
-	};
+};
 
-}
